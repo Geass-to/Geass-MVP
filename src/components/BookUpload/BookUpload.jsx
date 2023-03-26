a8454db6e7d85cf527f161625938063987733e73
Prasanth-BS <prasanthsubramanian19@gmail.com>
2023-03-26 22:27:40 +0530

    upload book skeleton

--- src/components/BookUpload/BookUpload.jsx
+++ src/components/BookUpload/BookUpload.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
+import "../../styles/bookupload.css"
 
 const BookUpload = () => {
 
 
-
+   
   return (
-     <>
-      <div>Book Upload</div>
-      <input type="text" /><br />
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-    </>
+    <div>
+      <form>
+        <ul className='upload-form'>
+          <li className='upload-form-field'>
+            Select the AudioFile: 
+            <input type="file" name="audioFile" id="audioFile" />
+          </li>
+
+          <li className='upload-form-field'>
+            Book title:
+            <input type="text" name="bookTitle" id="bookTitle" />
+          </li>
+
+          <li className='upload-form-field'>
+            Book Author:
+            <input type="text" name="" id="" />
+          </li>
+
+          <li className='upload-form-field'>
+            Give a brief description about the book:
+            <textarea name="bookDesc" id="bookDesc" cols="30" rows="10"></textarea>
+          </li>
+        </ul>
+
+      </form>
+    </div>
   )
 }
 
