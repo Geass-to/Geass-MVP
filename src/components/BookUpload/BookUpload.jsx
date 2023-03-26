c7ae721b71858dd6b2465252b3aaa60de4385f5b
Prasanth-BS <prasanthsubramanian19@gmail.com>
2023-03-26 23:36:21 +0530

    upload-book design#2

--- src/components/BookUpload/BookUpload.jsx
+++ src/components/BookUpload/BookUpload.jsx
@@ -37,25 +37,25 @@ const BookUpload = () => {
         <ul className='upload-form'>
           <li className='upload-form-field'>
             <span>Select the AudioFile:</span> 
-            <input type="file" name="audioFile" id="audioFile" />
+            <input type="file" name="audioFile" id="audioFile" required onChange={(e) => setAudioFile(e.target.value)} />
           </li>
 
           <li className='upload-form-field'>
             <span>Book title:</span>
-            <input type="text" name="bookTitle" id="bookTitle" />
+            <input type="text" name="bookTitle" id="bookTitle" required value={bookTitle} onChange={(e) => setBookTitle(e.target.value)} />
           </li>
 
           <li className='upload-form-field'>
             <span>Book Author:</span> 
-            <input type="text" name="" id="" />
+            <input type="text" name="" id="" value={bookAuthor} required onChange={(e) => setBookAuthor(e.target.value)} />
           </li>
 
           <li className='upload-form-field'>
             Give a brief description about the book:
-            <textarea name="bookDesc" id="bookDesc" cols="30" rows="10"></textarea>
+            <textarea name="bookDesc" id="bookDesc" cols="30" rows="10" value={bookDesc} required onChange={(e) => setBookDesc(e.target.value)}></textarea>
           </li>
+          <button type="submit">Submit</button>
         </ul>
-        <button type="submit">Submit</button>
       </form>
     </div>
   )
